da57a4c2eace90938ec64ef51ed91a9dd6c6b975
Philippe Métais <phi.metais@gmail.com>
2019-03-09 20:37:06 +0100

    :sparkles: Add /user and /claim routes

--- server/router.go
+++ server/router.go
@@ -19,5 +19,25 @@ func NewRouter() *gin.Engine {
 		utilGroup.GET("/health", uc.GetHealth)
 	}
 
+	userGroup := router.Group("/user")
+	{
+		uc := controllers.UserController{}
+		userGroup.POST("", uc.PostUser)
+		userGroup.POST("/search", uc.SearchUser)
+		userGroup.POST("/search/count", uc.CountUser)
+		userGroup.GET("/:id", uc.GetUser)
+		userGroup.PUT("/:id", uc.PutUser)
+	}
+
+	claimGroup := router.Group("/claim")
+	{
+		uc := controllers.ClaimController{}
+		claimGroup.POST("", uc.PostClaim)
+		claimGroup.POST("/search", uc.SearchClaim)
+		claimGroup.POST("/search/count", uc.CountClaim)
+		claimGroup.GET("/:id", uc.GetClaim)
+		claimGroup.PUT("/:id", uc.PutClaim)
+	}
+
 	return router
 }
